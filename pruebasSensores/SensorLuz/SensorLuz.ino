0da0bb210c32718330a92bf4cc2d420a10ff8e05
NicolasReyAlonso <nicolasreyalonso@gmail.com>
2026-01-10 14:57:31 +0000

    ADD ESQUEMA

---
+++ pruebasSensores/SensorLuz/SensorLuz.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int lectura = analogRead(A0);
+  Serial.println(lectura);
+  delay(200);
+}
